3b9feee95e5608c678256c1ed5b63ff0fbd84023
Aviral Bansal <56379558+aviral-bansal@users.noreply.github.com>
2021-06-15 22:23:22 +0530

    Invoice Generator App

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,13 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBtX0zbq8OWFoczz-D55IEQ-zQxcmjZ6BU",
-  authDomain: "invoice-app-yt.firebaseapp.com",
-  projectId: "invoice-app-yt",
-  storageBucket: "invoice-app-yt.appspot.com",
-  messagingSenderId: "638958069840",
-  appId: "1:638958069840:web:2a359ba0cf4b8c7b648955",
+const firebaseConfig = {
+  apiKey: "AIzaSyDBd_ryEoM26_LUk5y4Ap7SS5letn3Z4Vg",
+  authDomain: "vue-invoice-app-efe09.firebaseapp.com",
+  projectId: "vue-invoice-app-efe09",
+  storageBucket: "vue-invoice-app-efe09.appspot.com",
+  messagingSenderId: "865774560989",
+  appId: "1:865774560989:web:65f1c382398c21f2295d34"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
